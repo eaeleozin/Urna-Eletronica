9109322c4427072d2587e0577674ba1bf54bb1a4
leonardo.petrolli <leonardo.petrolli@valecard.com.br>
2022-07-12 16:25:21 -0300

    Add pastas e arquivos do projeto Urna-Eletronica

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '38111',
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'img47.png.jpeg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'img47.png.jpeg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'img47.png.jpeg', legenda: 'Prefeito' },
+                    { url: 'img47.png.jpeg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: 'img47.png.jpeg', legenda: 'Prefeito' },
+                    { url: 'img47.png.jpeg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    },
+];
